f6fd4b16824fc4d62f98171db1a708a54cd33b28
Joash <njovujsh@gmail.com>
2020-04-10 10:16:54 +0300

    first assignment trail

---
+++ build/lib/src/estimator.py
@@ -0,0 +1,6 @@
+import json
+
+def estimator(data):
+  app_json = json.dumps(data)
+  print(app_json)
+  return data
--- src/estimator.py
+++ src/estimator.py
@@ -1,6 +1,31 @@
 import json
+import sys
+from helpers import timenormaliseday
 
-def estimator(data):
-  app_json = json.dumps(data)
+
+data_json =  '{"region":{"name":"Africa","avgAge":19.7,"avgDailyIncomeInUSD":5,"avgDailyIncomePopulation":0.71}, "periodType":"days","timeToElapse":58,"reportedCases":674,"population":66622705,"totalHospitalBeds":1380614}'
+
+
+
+def estimator(data_input):
+  data= json.loads(data_input)
+  reportedCases=int(data["reportedCases"])
+  impact= {
+  'data':data, #the input data you got
+  'impact':{'currentlyInfected':reportedCases*10}, #your best case estimation,
+  'severeImpact':{'currentlyInfected':reportedCases*50} #your severe case estimationl,
+  }
+  factor=int(timenormaliseday(data['periodType'],data['timeToElapse'])*1/3)
+  # Edited output by number of days per infected people
+  impact['impact']['infectionsByRequestedTime']=impact['impact']['currentlyInfected']*2^factor
+  impact['severeImpact']['infectionsByRequestedTime']=impact['severeImpact']['currentlyInfected']*2^factor
+#   impact['infectionsByRequestedTime']={
+#     'impact':impact['impact']['currentlyInfected']*2^factor,
+#     'severeImpact':impact['severeImpact']['currentlyInfected']*2^factor
+#   }   
+  app_json = json.dumps(impact)
   print(app_json)
-  return data
+  return impact
+
+
+estimator(data_json)
