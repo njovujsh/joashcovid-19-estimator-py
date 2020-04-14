9e341efa0fc667574d907114d2ddb9521b3aac06
joash <njovujsh@gmail.com>
2020-04-14 13:10:22 +0300

    json challange two

--- src/estimator.py
+++ src/estimator.py
@@ -6,7 +6,7 @@ from src.helpers import timenormaliseday
 data_jsonx =  '{"region":{"name":"Africa","avgAge":19.7,"avgDailyIncomeInUSD":5,"avgDailyIncomePopulation":0.71}, "periodType":"days","timeToElapse":58,"reportedCases":674,"population":66622705,"totalHospitalBeds":1380614}'
 
 def estimator(data):
-  data=json.loads(data)
+#   data=json.loads(data)
   reportedCases=data["reportedCases"]
   impact= {
   'data':data, #the input data you got
