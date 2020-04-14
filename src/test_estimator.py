6fba1a15baf686c39ad6ebd36b01547f4bb6b9c6
joash <njovujsh@gmail.com>
2020-04-14 21:15:22 +0300

    tests trial

--- src/test_estimator.py
+++ src/test_estimator.py
@@ -1,6 +1,6 @@
 
 # from estimator import sumnumbers
-from estimator import estimator
+from src.estimator import estimator
 def test_estimator():
     #sample data from sample output
     data_json_sample= {'region': {'name': 'Africa', 'avgAge': 19.7, 'avgDailyIncomeInUSD': 4, 'avgDailyIncomePopulation': 0.73}, 'periodType': 'days', 'timeToElapse': 38, 'reportedCases': 2747, 'population': 92931687, 'totalHospitalBeds': 678874}
@@ -29,4 +29,5 @@ def test_sum():
 
 if __name__ == "__main__":
     test_sum()
+    test_estimator()
     print("Everything passed")
