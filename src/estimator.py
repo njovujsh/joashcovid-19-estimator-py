e2f2c4be290fe4b0c923d046345749c67826bf8e
joash <njovujsh@gmail.com>
2020-04-14 15:12:26 +0300

    json breaking

--- src/estimator.py
+++ src/estimator.py
@@ -1,61 +1,9 @@
-import json
 import sys
 from src.helpers import timenormaliseday
 
 
-data_jsonx =  '{"region":{"name":"Africa","avgAge":19.7,"avgDailyIncomeInUSD":5,"avgDailyIncomePopulation":0.71}, "periodType":"days","timeToElapse":58,"reportedCases":674,"population":66622705,"totalHospitalBeds":1380614}'
-
-def estimator_joshua(data):
-#   data=json.loads(data)
-  reportedCases=data["reportedCases"]
-  impact= {
-  'data':data, #the input data you got
-  'impact':{'currentlyInfected':reportedCases*10}, #your best case estimation,
-  'severeImpact':{'currentlyInfected':reportedCases*50} #your severe case estimationl,
-  }
-#   impact["estimate"]["impact"]["currentlyInfected"]=1
-  factor=int(timenormaliseday(data['periodType'],data['timeToElapse'])*1/3)
-  # Edited output by number of days per infected people
-  #Challenge 1
-  impact['impact']['infectionsByRequestedTime']=impact['impact']['currentlyInfected']*(2**factor)
-  impact['severeImpact']['infectionsByRequestedTime']=impact['severeImpact']['currentlyInfected']*(2**factor)
-
-  #severe cases by requested time  
-  impact['impact']['severeCasesByRequestedTime']=int(impact['impact']['infectionsByRequestedTime']*(15/100))
-  impact['severeImpact']['severeCasesByRequestedTime']=int(impact['severeImpact']['infectionsByRequestedTime']*(15/100))
-
-  #hospital beds by requested time
-  expectedtotalHospitalBeds=int(data["totalHospitalBeds"]*(35/100)) #Assumption is 1 bed per person
-  impact['impact']['hospitalBedsByRequestedTime']=expectedtotalHospitalBeds-impact['impact']['severeCasesByRequestedTime']
-  impact['severeImpact']['hospitalBedsByRequestedTime']=expectedtotalHospitalBeds-impact['severeImpact']['severeCasesByRequestedTime']
-
-  #estimated number of severe positive cases that will require ICU care.
-  impact['impact']['casesForICUByRequestedTime']=int(impact['impact']['infectionsByRequestedTime']*(5/100))
-  impact['severeImpact']['casesForICUByRequestedTime']=int(impact['severeImpact']['infectionsByRequestedTime']*(5/100))
-
-  #This is the estimated number of severe positive cases that will require ventilators.
-  impact['impact']['casesForVentilatorsByRequestedTime']=int(impact['impact']['infectionsByRequestedTime']*(2/100))
-  impact['severeImpact']['casesForVentilatorsByRequestedTime']=int(impact['severeImpact']['infectionsByRequestedTime']*(2/100))
-   
-  #dollars in flight estimate
-  avgDailyIncomeInUSD=data['region']["avgDailyIncomeInUSD"]
-  avgDailyIncomePopulation=data['region']["avgDailyIncomePopulation"]
-  timeindays=int(timenormaliseday(data['periodType'],data['timeToElapse']))
-
-  impact['impact']['dollarsInFlight']=int(impact['impact']['infectionsByRequestedTime']*(avgDailyIncomeInUSD*avgDailyIncomePopulation*timeindays))
-  impact['severeImpact']['dollarsInFlight']=int(impact['severeImpact']['infectionsByRequestedTime']*(avgDailyIncomeInUSD*avgDailyIncomePopulation*timeindays))
-
-
-  impact["estimate"] = {'impact':impact['impact'], 'severeImpact':impact['severeImpact']} 
-  del impact["impact"]
-  del impact["severeImpact"]
-#   app_json = json.dumps(impact)
-  return impact
-
-
 def estimator(data):
-  # data=json.loads(data)
-  reportedCases=data["reportedCases"]
+  reportedCases = data["reportedCases"]
   impact= {
   'data':data, #the input data you got
   'impact':{'currentlyInfected':reportedCases*10}, #your best case estimation,
@@ -66,32 +14,4 @@ def estimator(data):
   #Challenge 1
   impact['impact']['infectionsByRequestedTime']=impact['impact']['currentlyInfected']*(2**factor)
   impact['severeImpact']['infectionsByRequestedTime']=impact['severeImpact']['currentlyInfected']*(2**factor)
-
-  #severe cases by requested time  
-  impact['impact']['severeCasesByRequestedTime']=int(impact['impact']['infectionsByRequestedTime']*(15/100))
-  impact['severeImpact']['severeCasesByRequestedTime']=int(impact['severeImpact']['infectionsByRequestedTime']*(15/100))
-
-  #hospital beds by requested time
-  expectedtotalHospitalBeds=int(data["totalHospitalBeds"]*(35/100)) #Assumption is 1 bed per person
-  impact['impact']['hospitalBedsByRequestedTime']=expectedtotalHospitalBeds-impact['impact']['severeCasesByRequestedTime']
-  impact['severeImpact']['hospitalBedsByRequestedTime']=expectedtotalHospitalBeds-impact['severeImpact']['severeCasesByRequestedTime']
-
-  #estimated number of severe positive cases that will require ICU care.
-  impact['impact']['casesForICUByRequestedTime']=int(impact['impact']['infectionsByRequestedTime']*(5/100))
-  impact['severeImpact']['casesForICUByRequestedTime']=int(impact['severeImpact']['infectionsByRequestedTime']*(5/100))
-
-  #This is the estimated number of severe positive cases that will require ventilators.
-  impact['impact']['casesForVentilatorsByRequestedTime']=int(impact['impact']['infectionsByRequestedTime']*(2/100))
-  impact['severeImpact']['casesForVentilatorsByRequestedTime']=int(impact['severeImpact']['infectionsByRequestedTime']*(2/100))
-
-  #dollars in flight estimate
-  avgDailyIncomeInUSD=data['region']["avgDailyIncomeInUSD"]
-  avgDailyIncomePopulation=data['region']["avgDailyIncomePopulation"]
-  timeindays=int(timenormaliseday(data['periodType'],data['timeToElapse']))
-
-  impact['impact']['dollarsInFlight']=int(impact['impact']['infectionsByRequestedTime']*(avgDailyIncomeInUSD*avgDailyIncomePopulation*timeindays))
-  impact['severeImpact']['dollarsInFlight']=int(impact['severeImpact']['infectionsByRequestedTime']*(avgDailyIncomeInUSD*avgDailyIncomePopulation*timeindays))
-
-  app_json = json.dumps(impact)
-  # print(app_json)
-  return app_json
+  return impact
--- src/helpers.py
+++ src/helpers.py
@@ -9,12 +9,12 @@ def timenormaliseday(periodType,timeToElapse):
     dayvalue=0
     if periodType=="days" or periodType=="day":
        dayvalue=timeToElapse
-    elif len(daysen)== 0:
-       dayvalue=0
     elif periodType=="weeks" or periodType=="week" and len(daysen)!= 0:
        dayvalue=timeToElapse*7 #convert weeks to days 
     elif periodType=="month" or periodType=="months" and len(daysen)!= 0:
        dayvalue=timeToElapse*30 #convert month to days assuming the standard months are 30 
+    else:
+       dayvalue=0
     return dayvalue
 
 #Read Settings Files
